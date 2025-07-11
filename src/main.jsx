e5eb8e640a11d4138af96f890e72e466d1573dbf
fathrdm <114979588+fathrdm@users.noreply.github.com>
2025-07-11 13:34:19 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom';
+
 import './index.css'
 import App from './App.jsx'
 import Navbar from '../components/navbar.jsx'
@@ -9,13 +11,16 @@ import Projects from '../components/Projects.jsx'
 import Sertifikat from '../components/Sertifikat.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='container mx-auto px-5 lg:px-30'>
-      <Navbar />
-      <App />
-      <About/>
-      <Tools/>
-      <Projects/>
-      <Sertifikat/>
-    </div>
+    <Router>
+      <div className='container mx-auto px-5 lg:px-30'>
+        <Navbar />
+        <App />
+        <About />
+        <Tools />
+        <Projects />
+        <Sertifikat />
+      </div>
+
+    </Router>
   </StrictMode>,
 )
